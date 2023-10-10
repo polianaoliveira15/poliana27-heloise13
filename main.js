a34a55f3ae566c24d0ebb42e83aca2e980ac1881
polianaoliveira15 <105360435+polianaoliveira15@users.noreply.github.com>
2023-10-10 11:13:44 -0300

    ok

--- main.js
+++ main.js
@@ -2,3 +2,7 @@ function tocaSomPom(){
     document.querySelector('#som_tecla_pom').play();
 }
 document.querySelector('.tecla_pom').onclick = tocaSomPom;
+function tocaSomPom(){
+    document.querySelector('#som_tecla_Clap').play();
+}
+document.querySelector('.tecla_Clap').onclick = tocaSomClap;
