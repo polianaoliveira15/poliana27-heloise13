ac9f9b614691e03014667f4975a083e90eac75b3
polianaoliveira15 <105360435+polianaoliveira15@users.noreply.github.com>
2023-10-17 11:07:32 -0300

    ok

--- main.js
+++ main.js
@@ -3,12 +3,10 @@ function tocaSomPom(){
     document.querySelector("#som_tecla_pom").play();
 }
 //Invocando a função tocaSomPom pelo clique do botão Pom
-
     document.querySelector(".tecla_pom").onclick = tocaSomPom;
-//Criando referência constante ListaDeTeclas com buscando todos os itens pela classe .tecla
 
+//Criando referência constante ListaDeTeclas com buscando todos os itens pela classe .tecla
 const listaDeTeclas = document.querySelectorAll(".tecla");
 //Invocando a função tocaSomPom a partir do item "0". 1°botão.
-
 listaDeTeclas[0].onclick = tocaSomPom;
 
