56c995981cf017ac666dad9c3394cc0bf6c2bb04
polianaoliveira15 <105360435+polianaoliveira15@users.noreply.github.com>
2023-10-17 10:56:03 -0300

    ok

--- main.js
+++ main.js
@@ -1,10 +1,9 @@
+//Declarando/Criando a função tocaSomPom 
 function tocaSomPom(){
     document.querySelector('#som_tecla_pom').play();
 }
 document.querySelector('.tecla_pom').onclick = tocaSomPom;
-function tocaSomClap(){
-    document.querySelector('#som_tecla_clap').play();
-}
-document.querySelector('.tecla_clap').onclick = tocaSomClap;
 
-const listaDeTeclas = document.querySelectorAll('.tecla'_)
+const listaDeTeclas = document.querySelectorAll('.tecla');
+listaDeTeclas[0].onclick = tocaSomPom;
+
