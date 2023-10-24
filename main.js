6063bfae9198e6a0d550191c9eb843670ccca284
polianaoliveira15 <105360435+polianaoliveira15@users.noreply.github.com>
2023-10-24 11:27:29 -0300

    ok

--- main.js
+++ main.js
@@ -4,8 +4,8 @@ function tocaSom(idElementoAudio){
 }
 //Criando referência constante ListaDeTeclas com buscando todos os itens pela classe .tecla
 const listaDeTeclas = document.querySelectorAll(".tecla");
-//Invocando a função tocaSomPom a partir do item "0". 1°botão.
-listaDeTeclas[0].onclick = tocaSomPom;
+//Invocando a função tocaSom a partir do item "0". 1°botão.
+listaDeTeclas[0].onclick = tocaSom;
 
 //Criando referência variável "contador" iniciando = 0
 let contador = 0;
