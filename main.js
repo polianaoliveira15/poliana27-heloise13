7e44e7ab8515d0eddb2cd5ab451567461834c487
polianaoliveira15 <105360435+polianaoliveira15@users.noreply.github.com>
2023-11-08 09:29:48 -0300

    ok

--- main.js
+++ main.js
@@ -8,16 +8,15 @@ const listaDeTeclas = document.querySelectorAll(".tecla");
 listaDeTeclas[0].onclick = tocaSom;
 
 //Criando referência variável "contador" iniciando = 0
-let contador = 0;
 
-//Estrutura de repetição "white" significa "enquanto algo acontecer"
-while(contador <  listaDeTeclas. length);{
-    const efeito = listaDeTeclas[contador].classList(1);
-    const idAudio = "#som_"+ efeito;
-    listaDeTeclas[contador].onclick = function(){
+
+//Estrutura de repetição "white" significa
+//"Enquanto algo acontecer" mudopu while para "for"
+for(let contador = 0;contador <  listaDeTeclas. length;contador++);{
+    const tecla = listaDeTeclas[contador];
+    const efeito = listaDeTeclas[contador].classList[1]; //Acessa o 2° item da classe de teclas 
+    const idAudio = `#som_$(efeito)`; //Template string
+    tecla.onclick = function(){ //Cria função anônima para chamar tocaSom
         tocaSom(idAudio);
     }
-    contador = contador + 1;
-    console.log(contador);
-    
 }
