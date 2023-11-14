7b9da2c0359e206f8f54f6f6ea09ec0f0df5b000
polianaoliveira15 <105360435+polianaoliveira15@users.noreply.github.com>
2023-11-14 10:57:02 -0300

    ok

--- main.js
+++ main.js
@@ -4,17 +4,10 @@ function tocaSom(idElementoAudio){
 }
 //Criando referência constante ListaDeTeclas com buscando todos os itens pela classe .tecla
 const listaDeTeclas = document.querySelectorAll(".tecla");
-//Invocando a função tocaSom a partir do item "0". 1°botão.
-listaDeTeclas[0].onclick = tocaSom;
-
-//Criando referência variável "contador" iniciando = 0
-
-
-//Estrutura de repetição "white" significa
-//"Enquanto algo acontecer" mudopu while para "for"
+//Estrutura de repeição "while" significa "enquanto algo acontecer" mudou while para "for"
 for(let contador = 0;contador <  listaDeTeclas. length;contador++);{
     const tecla = listaDeTeclas[contador];
-    const efeito = listaDeTeclas[contador].classList[1]; //Acessa o 2° item da classe de teclas 
+    const efeito = tecla.classList[1]; //Acessa o 2° item da classe de teclas 
     const idAudio = `#som_$(efeito)`; //Template string
     tecla.onclick = function(){ //Cria função anônima para chamar tocaSom
         tocaSom(idAudio);
