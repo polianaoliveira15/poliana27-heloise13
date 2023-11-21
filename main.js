cf38448b84dae7db4b332b0096cbaf9ea85d18a6
polianaoliveira15 <105360435+polianaoliveira15@users.noreply.github.com>
2023-11-21 11:30:58 -0300

    ok

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 //Declarando/Criando a função tocaSom com paramêtro idElementoAudio
 function tocaSom(idElementoAudio){
     //if else e elemento operadores comparação
-    if (elemento && elemento.localName "audio"){
+    if elemento && elemento.localName ("audio"){
     const elemento = document.querySelector (idElementoAudio)
     elemento.play();
     }else{
