dc50f2a7b8d6f61ec70ca3f8f05bb3a0099fadca
polianaoliveira15 <105360435+polianaoliveira15@users.noreply.github.com>
2023-11-21 11:29:17 -0300

    ok

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ function tocaSom(idElementoAudio){
 //Criando referência constante ListaDeTeclas com buscando todos os itens pela classe .tecla
 const listaDeTeclas = document.querySelectorAll(".tecla");
 //Estrutura de repeição "while" significa "enquanto algo acontecer" mudou while para "for"
-for(let contador = 0;contador <  listaDeTeclas. length;contador++);{
+for(let contador = 0;contador <  listaDeTeclas.length;contador++) {
     const tecla = listaDeTeclas[contador];
     const efeito = tecla.classList[1]; //Acessa o 2° item da classe de teclas 
     const idAudio = `#som_$(efeito)`; //Template string
